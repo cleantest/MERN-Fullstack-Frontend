0f4965817ec53d46264edc0bd375d23c7e3a4716
cleantest <brianmuriuki475@gmail.com>
2025-12-02 13:25:40 +0400

    FInalized comments

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ new Vue({
         break;
       case 'location-desc':
         results.sort((a, b) =>
-          b.location.toLowerCase().localeCompare(a.location.toLowerCase())
+          a.location.toLowerCase().localeCompare(b.location.toLowerCase()) * -1
         );
         break;
         case 'subject-asc':
